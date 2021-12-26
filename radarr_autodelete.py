c80356f1a122651b0e1220f24a8e768c95b5e64d
Jan Chroback <jan.chroback@protonmail.com>
2021-12-26 20:31:09 +0100

    datetime is now properly converted to full seconds ints

--- radarr_autodelete.py
+++ radarr_autodelete.py
@@ -1,17 +1,24 @@
 import os
+from datetime import datetime
 from dotenv import load_dotenv
 from pyarr import RadarrAPI
 
 def is_movie_tagged(movie, filtertag):
     tags = movie['tags']
     if tags != []:
-        print('Found Tagged Movie: ' + movie['title'])
         for tag in tags:
             texttag = radarr.get_tag(tag)
             if texttag['label'] == filtertag:
                 return True
     else:
         return False
+
+def should_movie_delete(movie, currentTime):
+    added = movie['added']
+    unifiedAdded = added.split('T', 1)[0]
+    dateAddedToDatetime = datetime.strptime(unifiedAdded, '%Y-%m-%d')
+    dateAddedInSeconds = int(dateAddedToDatetime.timestamp())
+    print(dateAddedInSeconds)
 
 load_dotenv()
 
@@ -23,8 +30,12 @@ radarr = RadarrAPI(host_url, api_key);
 
 movies = radarr.get_movie();
 
+
+dt = datetime.today() 
+secondsNow = int(dt.timestamp())
+print(secondsNow)
 for movie in movies:
-    result = is_movie_tagged(movie, 'theaterslist')
-    if result == True:
-        print('movie is tagged')
+    tagged_status = is_movie_tagged(movie, 'theaterslist')
+    if tagged_status == True:
+        deletable = should_movie_delete(movie, secondsNow)
 
